bff8f582e479814d2f2cdcc6e5e737f095546734
pjBieniek <p.j.bieniek@wp.pl>
2020-02-12 12:01:47 +0100

    ConfigureServices Startup for Swagger

--- SolarApp/Startup.cs
+++ SolarApp/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SolarApp.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace SolarApp
 {
@@ -21,13 +23,20 @@ namespace SolarApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            
+            // Register the Swagger generator
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new OpenApiInfo { Title = "Solar API", Version = "v1" });
             });
         }
 
@@ -48,7 +57,11 @@ namespace SolarApp
             var swaggerOptions = new SwaggerOptions();
             Configuration.GetSection(nameof(SwaggerOptions)).Bind(swaggerOptions);
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger(option => { option.RouteTemplate = swaggerOptions.JsonRoute;});
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(option => { option.SwaggerEndpoint(swaggerOptions.UiEndPoint, swaggerOptions.Desription); });
             app.UseHttpsRedirection();
             app.UseStaticFiles();
