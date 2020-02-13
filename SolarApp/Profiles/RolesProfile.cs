fb662df4293ffca56a379489f5a21a565b04cae2
pjBieniek <p.j.bieniek@wp.pl>
2020-02-13 13:31:16 +0100

    Add Profile classess

---
+++ SolarApp/Profiles/RolesProfile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolarApp.Profiles
+{
+    public class RolesProfile
+    {
+    }
+}
