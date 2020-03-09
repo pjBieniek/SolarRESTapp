55e68624325019749f10c22076e1eec3653df4b5
pjBieniek <p.j.bieniek@wp.pl>
2020-03-09 09:19:20 +0100

    Solution structure: Tests refactor

--- SolarApp.Tests/Controllers/CompetitionsControllerTests.cs
+++ SolarApp.Tests/Controllers/CompetitionsControllerTests.cs
@@ -6,7 +6,7 @@ using System.Net;
 
 using System.Threading.Tasks;
 
-namespace NUnitTestSolarAPI.Controllers
+namespace SolarApp.Tests.Controllers
 {
     class CompetitionsControllerTests : IntegrationTest
     {
