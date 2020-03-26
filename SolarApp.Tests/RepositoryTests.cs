1d953913320886a407ce178df36a15dc7266a2dc
pjBieniek <p.j.bieniek@wp.pl>
2020-03-26 12:38:07 +0100

    dbRepository tests refactor

---
+++ SolarApp.Tests/RepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarApp.Data.Services;
+using SolarApp.DatabaseCreation.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarApp.Tests
+{
+    class RepositoryTests
+    {
+        protected SolarDbRepository repo;
+        protected SolarDbContext context;
+
+        public RepositoryTests()
+        {
+            var repositoryOptions = new DbContextOptionsBuilder<SolarDbContext>()
+                    .UseInMemoryDatabase("Test db1")
+                    .Options;
+            context = new SolarDbContext(repositoryOptions);
+            repo = new SolarDbRepository(context);
+        }
+    }
+}
